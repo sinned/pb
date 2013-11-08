183c19feb71ad6d6ebc6d95a8710d37969fe8d9f
Dennis Yang <sinned@gmail.com>
2013-11-08 14:53:15 -0800

    fixing pages;

--- blog/wp-content/themes/subcomm/page.php
+++ blog/wp-content/themes/subcomm/page.php
@@ -1,12 +1,18 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : ?>
 
-	<?php /* The loop */ ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<h3><?php echo the_title(); ?></h3>
-		<p><?php echo the_content(); ?></p>
+	<section id="mainContent">
+		<div class="row">
+			<div class="large-12 columns">
+				<?php /* The loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<h1><?php echo the_title(); ?></h1>
+					<p><?php echo the_content(); ?></p>
 
-	<?php endwhile; ?>
+				<?php endwhile; ?>
+			</div>
+		</div>
+	</section?
 
 <?php else : ?>
 	<p>No posts to show.</p>
