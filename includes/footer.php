2f203e8f7143b01ec541c4a87c2ef9ff88db32c0
Dennis Yang <sinned@gmail.com>
2013-11-13 17:22:40 -0800

    adding mailchimp form to footer.

--- includes/footer.php
+++ includes/footer.php
@@ -17,9 +17,27 @@
 				<li><a href="http://www.pinterest.com/getpoopbuddy"><img src="<?php echo WEBROOT; ?>img/pinterest-green.png" /></a></li>
 				<li><a href="http://www.instagram.com/getpoopbuddy"><img src="<?php echo WEBROOT; ?>img/instagram-green.png" /></a></li>				
 			</ul>
+			<br style="clear:both;"/>
+
+	        <!-- Begin MailChimp Signup Form -->
+			<div class="right" id="mc_embed_signup2">
+			<form action="http://poopbuddy.us7.list-manage.com/subscribe/post?u=7cccd32f632de3c4496407d9b&amp;id=4caf43e792" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<label for="mce-EMAIL">Join for 20% Off</label>
+				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+			    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="small button" style='background:#fcb318 !important; color:#fff;'>
+			</form>
+			</div>
+			<!--End mc_embed_signup-->				
 
 			<br style="clear:both;"/>
 			<p>&copy;2013 Poop Buddy</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-3 column">
+		</div>
+		<div class="large-9 columns">
+		
 		</div>
 	</div>
 </section>
