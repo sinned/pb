f12244326868c41a3305f31011b7beef70a12522
Dennis Yang <sinned@gmail.com>
2013-11-13 18:27:46 -0800

    adding GA tracking widget

--- includes/scripts.php
+++ includes/scripts.php
@@ -21,3 +21,14 @@ switch ($_SERVER['SERVER_NAME']) {
 <!-- END FOXYCART FILES -->
 
 <!-- start Dropifi --> <script type='text/javascript' src='https://s3.amazonaws.com/dropifi/js/widget/dropifi_widget.min.js'></script><script type='text/javascript'>document.renderDropifiWidget('f5b6a5f3cdb5a8370a48ef8ffa70a5ee-1384393592402');</script> <!-- end Dropifi -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41730942-1', 'poopbuddy.com');
+  ga('send', 'pageview');
+
+</script>
