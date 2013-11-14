462afde1b37fd3e1854184af2856f368cff0a296
Dennis Yang <sinned@gmail.com>
2013-11-13 18:37:10 -0800

    adding GA stuff

--- includes/scripts.php
+++ includes/scripts.php
@@ -32,3 +32,21 @@ switch ($_SERVER['SERVER_NAME']) {
   ga('send', 'pageview');
 
 </script>
+<script type="text/javascript" charset="utf-8">
+	fcc.events.cart.preprocess.add(function(e, arr) {
+		if (arr['cart'] == 'checkout' || arr['cart'] == 'updateinfo' || arr['output'] == 'json') {
+			return true;
+		}
+		if (arr['cart'] == 'checkout_paypal_express') {
+			_gaq.push(['_trackPageview', '/paypal_checkout']);
+			return true;
+		}
+		_gaq.push(['_trackPageview', '/cart']);
+		return true;
+	});
+	fcc.events.cart.process.add_pre(function(e, arr) {
+		var pageTracker = _gat._getTrackerByName();
+		jQuery.getJSON('https://' + storedomain + '/cart?' + fcc.session_get() + '&h:ga=' + escape(pageTracker._getLinkerUrl('', true)) + '&output=json&callback=?', function(data){});
+		return true;
+	});
+</script>
