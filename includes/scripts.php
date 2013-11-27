7da2a56209285860b806e95b4be12a0f32af164b
sinned <sinned@gmail.com>
2013-11-27 01:19:55 -0800

    adding /cart GA pageview

--- includes/scripts.php
+++ includes/scripts.php
@@ -53,26 +53,6 @@ switch ($_SERVER['SERVER_NAME']) {
  
 </script>
 
-<script type="text/javascript" charset="utf-8">
-fcc.events.cart.preprocess.add(function(e, arr) {
-  if (arr['cart'] == 'checkout' || arr['cart'] == 'updateinfo' || arr['output'] == 'json') {
-    return true;
-  }
-  if (arr['cart'] == 'checkout_paypal_express') {
-    _gaq.push(['_trackPageview', '/paypal_checkout']);
-    return true;
-  }
-  _gaq.push(['_trackPageview', '/cart']);
-  return true;
-});
-fcc.events.cart.process.add_pre(function(e, arr) {
-  var pageTracker = _gat._getTrackerByName();
-  jQuery.getJSON('https://' + storedomain + '/cart?' + fcc.session_get() + '&h:ga=' + escape(pageTracker._getLinkerUrl('', true)) + '&output=json&callback=?', function(data){});
-  return true;
-});
-</script>
-
-
 <script src="<?php echo WEBROOT; ?>js/vendor/jquery.cookie.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php echo WEBROOT; ?>js/vendor/jquery.bxslider.min.js" type="text/javascript" charset="utf-8"></script>
 
