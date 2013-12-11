6ae4d1451413e287a5d2d4d933c33965c46a65de
sinned <sinned@gmail.com>
2013-12-11 00:58:07 -0800

    testing the slider

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+	<title>Test</title>
+	<?php include "includes/scripts.php"; ?>
+	<script language="javascript">
+		$(document).ready(function(){
+			$('.bxslider').bxSlider();
+			console.log('moo');
+		});	
+	</script>
+</head>
+<body>
+<ul class="bxslider">
+  <li><img src="http://i.poopbuddy.com/img/450x-element-of-surprise.png" /></li>
+  <li><img src="http://i.poopbuddy.com/img/450x-poopbuddychristmas.png" /></li>
+  <li><img src="http://i.poopbuddy.com/img/PoopBuddy.png" /></li>
+</ul>
+</bdy>
