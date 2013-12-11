c0ff45f8838458e629a05c827ff8460befeb8c8d
sinned <sinned@gmail.com>
2013-12-11 00:23:50 -0800

    making work for ie8

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-;(function ($, window, undefined) {
-  'use strict';
-
-  var $doc = $(document),
-      Modernizr = window.Modernizr;
-
   $(document).ready(function() {
     $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
     $.fn.foundationButtons          ? $doc.foundationButtons() : null;
@@ -164,10 +158,10 @@
   });
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+  $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
   if (Modernizr.touch && !window.location.hash) {
@@ -178,4 +172,3 @@
     });
   }
 
-})(jQuery, this);
