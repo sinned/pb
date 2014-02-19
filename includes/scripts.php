7b24d5dcf0a9273de643c000d102dd5cc5e01702
Dennis Yang <sinned@gmail.com>
2014-02-19 15:05:13 -0800

    removing dropifi again

--- includes/scripts.php
+++ includes/scripts.php
@@ -50,5 +50,3 @@ switch ($_SERVER['SERVER_NAME']) {
 <link rel="stylesheet" href="//cdn.foxycart.com/static/scripts/colorbox/1.3.23/style1_fc/colorbox.css?ver=1" type="text/css" media="screen" charset="utf-8" />
 <script src="//cdn.foxycart.com/poopbuddy/foxycart.colorbox.js?ver=2" type="text/javascript" charset="utf-8"></script>
 <!-- END FOXYCART FILES -->
-
-<!-- start Dropifi --> <script type='text/javascript' src='https://s3.amazonaws.com/dropifi/js/widget/dropifi_widget.min.js'></script><script type='text/javascript'>document.renderDropifiWidget('f5b6a5f3cdb5a8370a48ef8ffa70a5ee-1384393592402');</script> <!-- end Dropifi -->
