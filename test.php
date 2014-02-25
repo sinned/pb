fe8a368908c3dc2cde5ec2ec6ecbaf792c983f31
sinned <sinned@gmail.com>
2014-02-25 13:18:42 -0800

    infusionsoft test

--- test.php
+++ test.php
@@ -1,23 +1,201 @@
-<!DOCTYPE html>
-<head>
-	<title>Test</title>
-	<?php include "includes/scripts.php"; ?>
-	<script language="javascript">
-		$(document).ready(function(){
 
-			    $('.slider1').bxSlider({
-			      minSlides: 1,
-			      maxSlides: 1,
-			      slideMargin: 0
-			    });  
-    			console.log('moo');
-		});	
-	</script>
-</head>
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width" />	
+	<title>PoopBuddy: Error</title>
+	<link rel="shortcut icon" href="http://i.poopbuddy.com/img/favicon.png" type="image/png" />
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/pb/stylesheets/custom.css" />
+<link rel="stylesheet" href="/pb/stylesheets/normalize.css" />
+<link rel="stylesheet" href="/pb/stylesheets/app.css" />
+<link rel="stylesheet" href="/pb/stylesheets/jquery.bxslider.css" />
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="/pb/js/vendor/custom.modernizr.js"></script>
+<!-- start Dropifi --> <script type='text/javascript' src='https://s3.amazonaws.com/dropifi/js/widget/dropifi_widget.min.js'></script><script type='text/javascript'>document.renderDropifiWidget('f5b6a5f3cdb5a8370a48ef8ffa70a5ee-1384393592402');</script> <!-- end Dropifi -->
+
+<!-- Foundation 3 for IE 8 and earlier -->
+<!--[if lt IE 9]>
+  <link rel="stylesheet" href="/pb/css/foundation3/normalize.css">
+  <link rel="stylesheet" href="/pb/css/foundation3/foundation.css">
+  <link rel="stylesheet" href="/pb/css/foundation3/app.css">
+<![endif]-->
+
+
+
+<script type="text/javascript">
+ 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-41730942-1']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_setAllowAnchor', true]);
+  _gaq.push(['_trackPageview']);
+ 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+ 
+</script>
+
+<script src="/pb/js/vendor/jquery.cookie.js" type="text/javascript" charset="utf-8"></script>
+<script src="/pb/js/vendor/jquery.bxslider.min.js" type="text/javascript" charset="utf-8"></script>
+
+<!-- BEGIN FOXYCART FILES -->
+<link rel="stylesheet" href="//cdn.foxycart.com/static/scripts/colorbox/1.3.23/style1_fc/colorbox.css?ver=1" type="text/css" media="screen" charset="utf-8" />
+<script src="//cdn.foxycart.com/poopbuddy/foxycart.colorbox.js?ver=2" type="text/javascript" charset="utf-8"></script>
+<!-- END FOXYCART FILES --></head>
 <body>
-<ul class="slider1">
-  <li><img src="http://i.poopbuddy.com/img/450x-element-of-surprise.png" /></li>
-  <li><img src="http://i.poopbuddy.com/img/450x-poopbuddychristmas.png" /></li>
-  <li><img src="http://i.poopbuddy.com/img/PoopBuddy.png" /></li>
-</ul>
-</bdy>
+	<div class="row">
+	<div class="large-12 column">
+		<div class="contain-to-grid text-center fixed">
+			<nav class="top-bar">
+			  <ul class="title-area">
+			    <!-- Title Area -->
+			    <li class="name">
+			    	<a href="/pb/"><img style="width:250px;" src="http://i.poopbuddy.com/img/poopbuddy-logo.png" alt="PoopBuddy" title="PoopBuddy" /></a>
+			    </li>
+			    <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+			    <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+			  </ul>
+			  <section class="top-bar-section">
+			    <ul class="right">
+					<li><a id="fc_minicart" href="https://poopbuddy.foxycart.com/cart?cart=view">
+						<span id="fc_quantity">0</span>
+						<span id="fc_singular"> item </span>
+						<span id="fc_plural"> items </span> in cart
+					</a></li>					
+					<li class="divider"></li>    	
+					<li><a href="http://www.poopbuddy.com/what-you-get/">What You Get</a></li>
+					<li class="divider"></li>    	
+					<li><a href="http://www.poopbuddy.com/buy/">Get Started</a></li>
+					<li class="divider"></li>    
+					<li><a href="http://store.poopbuddy.com/">Store</a></li>
+					<li class="divider"></li>    
+					<li><a href="/pb/blog/">Blog</a></li>					
+				</ul>
+			  </section>
+			</nav>		
+		</div>
+	</div>
+</div>	<section id="mainContent">
+		<div class="row">
+			<div class="large-12 columns" style="margin:50px 0;">
+			
+				<script type="text/javascript" src="https://ii171.infusionsoft.com/app/form/iframe/2d2b69ae383c9aed4c01facb5d660f79"></script>
+
+			</div>
+		</div>
+	</section>
+		
+	<div class="row">
+	<div class="large-12 columns text-center">
+		<img style="margin:-8px;" src="http://i.poopbuddy.com/img/doggy-peeking.png" alt="Peeking Dog" />
+	</div>
+</div>
+<section id="footer">
+	<div class="row">
+		<div class="large-12 column">
+			<ul class="inline-list left">
+				<li><a href="http://www.poopbuddy.com//about/">About</a></li>
+				<li><a href="http://www.poopbuddy.com//faq/">FAQ</a></li>
+				<li><a href="http://www.poopbuddy.com//press/">Press</a></li>
+				<li><a href="http://www.poopbuddy.com/blog/brand-partners/">Partners</a></li>
+			</ul>
+			<ul class="inline-list right socialmedia">
+				<li><a href="http://www.facebook.com/poopbuddy"><img src="http://i.poopbuddy.com/img/facebook-green.png" /></a></li>
+				<li><a href="http://www.twitter.com/getpoopbuddy"><img src="http://i.poopbuddy.com/img/twitter-green.png" /></a></li>
+				<li><a href="http://www.pinterest.com/getpoopbuddy"><img src="http://i.poopbuddy.com/img/pinterest-green.png" /></a></li>
+				<li><a href="http://www.instagram.com/getpoopbuddy"><img src="http://i.poopbuddy.com/img/instagram-green.png" /></a></li>				
+			</ul>
+			<br style="clear:both;"/>
+
+	        <!-- Begin MailChimp Signup Form -->
+			<div class="right" id="mc_embed_signup2">
+			<form action="http://poopbuddy.us7.list-manage.com/subscribe/post?u=7cccd32f632de3c4496407d9b&amp;id=4caf43e792" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<label for="mce-EMAIL">Join for news &amp; offers!</label>
+				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your email" required>
+			    <input type="submit" value="Sign Me Up!" name="subscribe" id="mc-embedded-subscribe" class="small button" style='background:#fcb318 !important; color:#fff;'>
+			</form>
+			</div>
+			<!--End mc_embed_signup-->				
+
+			<br style="clear:both;"/>
+			<p>&copy;2013 Poop Buddy</p>
+		</div>
+	</div>
+</section>
+
+<div id="squeezeModal" class="reveal-modal" style="max-width:800px; border:10px solid #02bdb8;background:url('/pb/img/squeeze.png') no-repeat right #fff">
+    <a class="close-reveal-modal">&#215;</a>
+
+    <div class="row">
+    	<div class="large-7 columns">
+    		<h1>Join Our Pack</h1>
+    		<p>Sign up here for special offers, contests and more.
+			<!-- Begin MailChimp Signup Form -->
+
+			<div id="mc_embed_signup">
+			<form action="http://poopbuddy.us7.list-manage2.com/subscribe/post?u=7cccd32f632de3c4496407d9b&amp;id=4caf43e792" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your email address" required>
+				<div class="clear"><input type="submit" value="Join!" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			</form>
+			</div>
+			<!--End mc_embed_signup-->
+		</div>
+		<div class="large-5 columns">
+		</div>
+	</div>
+</div>
+<div id="messageModal" class="reveal-modal">   
+	<a class="close-reveal-modal">&#215;</a>
+	<p></p>
+</div>
+
+<script>
+document.write('<script src=' +
+('__proto__' in {} ? '/pb/js/vendor/zepto' : '/pb/js/vendor/jquery') +
+'.js><\/script>')
+</script>
+
+<script type="text/javascript" charset="utf-8">
+fcc.events.cart.preprocess.add(function(e, arr) {
+if (arr['cart'] == 'checkout' || arr['cart'] == 'updateinfo' || arr['output'] == 'json') {
+  return true;
+}
+if (arr['cart'] == 'checkout_paypal_express') {
+  _gaq.push(['_trackPageview', '/paypal_checkout']);
+  return true;
+}
+_gaq.push(['_trackPageview', '/cart']);
+return true;
+});
+fcc.events.cart.process.add_pre(function(e, arr) {
+var pageTracker = _gat._getTrackerByName();
+jQuery.getJSON('https://' + storedomain + '/cart?' + fcc.session_get() + '&h:ga=' + escape(pageTracker._getLinkerUrl('', true)) + '&output=json&callback=?', function(data){});
+return true;
+});
+</script>
+
+<script src="/pb/js/app.js"></script>
+
+<!-- Foundation 3 for IE 8 and earlier -->
+<!--[if lt IE 9]>
+    <script src="/pb/js/foundation3/foundation.min.js"></script>
+    <script src="/pb/js/foundation3/app.js"></script>
+<![endif]-->
+
+<script src="http://i.poopbuddy.com/js/foundation.min.js"></script>
+<script type="text/javascript" async data-pin-hover="true" src="//assets.pinterest.com/js/pinit.js"></script>
+
+<script>
+$(document).foundation();
+</script>
+</body>
+</html>
